fc11e4502e5fe7fe4170a002b3159bfd0a5746a8
Zakir44 <79624334+Zakir44@users.noreply.github.com>
2021-06-25 00:48:38 +0600

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,18 @@
+J = float(input("Hey Give Your Grade: "))
+
+if(J == 5):
+    print ("Your Grade is A+")
+elif(J == 4):
+    print("Your Grade is A")
+elif(J==3.5):
+    print("Your Grade is A-")
+elif(J==3):
+    print("Your Grade is B")
+elif(J==2.5):
+    print("Your Grade is C")
+elif(J == 2):
+    print("Your Grade is D")
+elif(J<2):
+    print("Your Grade is F")
+else:
+    print("Opps! Sorry,Your Information is Wrong")
